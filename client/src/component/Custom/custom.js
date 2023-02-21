2d4324d4bb10ff4933da42dc76a4d3dca645ab55
Kayyasit Chumthong <6510110034@email.psu.ac.th>
2023-02-22 04:04:10 +0700

    add logout

--- client/src/component/Custom/custom.js
+++ client/src/component/Custom/custom.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Button } from "reactstrap";
 
 const Loginbar =() =>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const jwt =localStorage.getItem ("jwt")
+        setIsLoggedIn(jwt? true : false)
+    },[]);
+
+    const handdletLogout =() =>{
+        localStorage.removeItem("jwt")
+        setIsLoggedIn(false)
+        navigate('/')
+    }
     return <bar>
-        <a href="login">LOGIN</a>
+        {isLoggedIn ?(
+            <Button onClick={handdletLogout}>Logout</Button>
+        ):(
+            <a href="Login">Login</a>
+
+        )}
+
+        
     </bar>
+
 };
 
 export default Loginbar;
