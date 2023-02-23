41f549d1040db1e27d77685e9990e9debe408139
Chakkawan Khammanee <6510110061@psu.ac.th>
2023-02-23 13:26:04 +0700

    Login in public page

--- client/src/component/Custom/custom.js
+++ client/src/component/Custom/custom.js
@@ -1,6 +1,9 @@
+import { AppBar, Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { Button } from "reactstrap";
+import { Button, Card } from "reactstrap";
+import "./custom.css"
+import Logo from "./../images/circleNsc.png"
 
 const Loginbar =() =>{
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -11,17 +14,41 @@ const Loginbar =() =>{
         setIsLoggedIn(jwt? true : false)
     },[]);
 
-    const handdletLogout =() =>{
+    const handdletLogout = () => {
         localStorage.removeItem("jwt")
         setIsLoggedIn(false)
-        navigate('/logout')
+        navigate('/')
     }
+    
     return <bar>
         {isLoggedIn ?(
             <Button onClick={handdletLogout}>Logout</Button>
         ):(
-            <a href="Login">Login</a>
-
+            <Box>
+                <AppBar position="static" style={{padding: 20}}>
+                    <Card
+                        raised
+                        sx={{
+                            maxWidth: 280,
+                        margin: "0 auto",
+                        padding: "0.1em",
+                        }}>
+                        <div>
+                            <div className="image-container">
+                                <img className="potrait" src={require=(Logo)} align-item= "left" alt="Logo"/>
+                            <Typography variant="h6" component="div" fontFamily={"-apple-system"} sx={{ flexGrow: 1 }}>
+                                NSC Project
+                            </Typography>
+                                    <div className="button-container">
+                                        <Button className="login-button" color="inherit" onClick={() => navigate("/login/")}>
+                                            Login
+                                        </Button>
+                                    </div>
+                            </div>
+                        </div>
+                    </Card>
+                </AppBar>
+            </Box>
         )}
 
         
--- client/src/page/publicpage.js
+++ client/src/page/publicpage.js
@@ -1,6 +1,9 @@
+import Loginbar from '../component/Custom/custom'
 import './background.css'
 const Publicpage =()=>{
-    return(<div style={{textAlign: 'center', display:'contents', justifyContent: 'center',}}>
+    return(
+    <div style={{textAlign: 'center', display:'contents', justifyContent: 'center',}}>
+        <Loginbar/>
         <table style={{ margin: 'auto',tableLayout:'auto' }}>
             <tr>
                 <td style={{ border: '1px solid black', padding: '10px' }}>
@@ -111,8 +114,6 @@ const Publicpage =()=>{
             </tr>
         </table>
                 </div>
-          
-      
         
         
     )
