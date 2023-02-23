ee1a051e7e83c1850a48e490221145e21a17d37c
Chakkawan Khammanee <6510110061@psu.ac.th>
2023-02-23 17:03:47 +0700

    Public Page

--- client/src/component/Custom/custom.js
+++ client/src/component/Custom/custom.js
@@ -1,5 +1,5 @@
 import { AppBar, Box, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Text } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Button, Card } from "reactstrap";
 import "./custom.css"
@@ -25,22 +25,22 @@ const Loginbar =() =>{
             <Button onClick={handdletLogout}>Logout</Button>
         ):(
             <Box>
-                <AppBar position="static" style={{padding: 20}}>
+                <AppBar position="static" style={{padding:"20", background:'#2E3B55', maxHeight:"200vh"}}>
                     <Card
                         raised
                         sx={{
                             maxWidth: 280,
-                        margin: "0 auto",
-                        padding: "0.1em",
+                            margin: "0 auto",
+                            padding: "0.1 em",
                         }}>
                         <div>
                             <div className="image-container">
                                 <img className="potrait" src={require=(Logo)} align-item= "left" alt="Logo"/>
-                            <Typography variant="h6" component="div" fontFamily={"-apple-system"} sx={{ flexGrow: 1 }}>
-                                NSC Project
-                            </Typography>
+                                    <Typography textAlign={"left"} marginLeft={"100px"} marginBottom={"-90px"} marginTop={"-95px"} variant="h6" component="div" fontFamily={"Sukhumvit Set"} sx={{ flexGrow: 1 }}>
+                                        <p>NSC Southern Thailand Web 2022 <br/>เว็บศูนย์ประสานงานโครงการ NSC ภาคใต้</p>
+                                    </Typography>
                                     <div className="button-container">
-                                        <Button className="login-button" color="inherit" onClick={() => navigate("/login/")}>
+                                        <Button className="login-button" color="inherit" justify-items onClick={() => navigate("/login/")}>
                                             Login
                                         </Button>
                                     </div>
