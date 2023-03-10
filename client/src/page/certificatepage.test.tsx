f7bc254c2c8d8996880c0d08050a169223327b3d
Kayyasit Chumthong <6510110034@email.psu.ac.th>
2023-03-10 23:10:07 +0700

    fix

---
+++ client/src/page/certificatepage.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react';
+import Certificatepage from './certificatepage'
+
+    test('"ดาวน์โหลดใบประกาศนียบัตร"button', () => {
+        render(<Certificatepage />);
+        const checkStatusButton = screen.getByText(/ดาวน์โหลดใบประกาศนียบัตร/i);
+        expect(checkStatusButton).toBeInTheDocument();
+    });
+
+
+    test("ตัวอย่างใบประกาศนียบัตร", () => {
+        render(<Certificatepage />);
+        const checkStatusButton = screen.getByText(/ตัวอย่างใบประกาศนียบัตร/i);
+        expect(checkStatusButton).toBeInTheDocument();
+    });
